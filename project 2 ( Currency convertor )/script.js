5a0781b5d24e20a9e19b9ef915d6ed3709bd413d
Tushar <100889817+Tusharkakad03@users.noreply.github.com>
2024-05-25 19:07:50 +0530

    Add files via upload

---
+++ project 2 ( Currency convertor )/script.js
@@ -0,0 +1,8 @@
+
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json"
+
+let dropdowns = document.querySelectorAll(".dropdown selct");
+
+for(code in countryList){
+     console.log(code);
+}
